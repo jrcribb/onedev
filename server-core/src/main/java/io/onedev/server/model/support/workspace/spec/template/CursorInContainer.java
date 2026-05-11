d30926c0d76b5704c88640b078ba72c9fe5c1994
Robin Shen <robin@onedev.io>
2026-05-11 17:52:08 +0800

    feat: Add Cursor CLI workspace template (OD-2773)

---
+++ server-core/src/main/java/io/onedev/server/model/support/workspace/spec/template/CursorInContainer.java
@@ -0,0 +1,50 @@
+package io.onedev.server.model.support.workspace.spec.template;
+
+import java.util.List;
+
+import io.onedev.commons.codeassist.InputSuggestion;
+import io.onedev.server.annotation.Editable;
+import io.onedev.server.model.support.workspace.spec.ShortcutConfig;
+import io.onedev.server.model.support.workspace.spec.UserData;
+import io.onedev.server.model.support.workspace.spec.WorkspaceSpec;
+import io.onedev.server.model.support.workspace.spec.shell.CustomLinuxShell;
+import io.onedev.server.web.util.SuggestionUtils;
+
+@Editable(order=400, name="nt:Cursor CLI in Container", description="""
+        Create a workspace spec running Cursor CLI inside container for isolation and security purpose.
+        You may customize the <a href='https://code.onedev.io/onedev/docker/cursor' target='_blank'>container image</a> 
+        later to suit your needs if desired""")
+public class CursorInContainer extends WorkspaceSpecTemplate {
+
+    @Override
+    public WorkspaceSpec createWorkspaceSpec() {
+        var workspaceSpec = new WorkspaceSpec();
+
+        workspaceSpec.setName(getName());
+        workspaceSpec.setRunInContainer(true);
+        workspaceSpec.setImage("1dev/cursor");
+        workspaceSpec.setShell(new CustomLinuxShell());
+        workspaceSpec.setRunAs("1001:1001");
+
+        var shortcutConfig = new ShortcutConfig();
+        shortcutConfig.setName("Cursor CLI");
+        shortcutConfig.setCommand("agent");
+        workspaceSpec.getShortcutConfigs().add(shortcutConfig);
+
+        var userData = new UserData();
+        userData.setKey("cursor");
+        userData.getPaths().add("/home/cursor/.profile");
+        userData.getPaths().add("/home/cursor/.bashrc");
+        userData.getPaths().add("/home/cursor/.cursor");
+        userData.getPaths().add("/home/cursor/.config/cursor");
+        workspaceSpec.getUserDatas().add(userData);
+
+        return workspaceSpec;
+    }
+
+	@SuppressWarnings("unused")
+	private static List<InputSuggestion> suggestVariables(String matchWith) {
+		return SuggestionUtils.suggestWorkspaceVariables(matchWith);
+	}
+
+}
---
+++ server-core/src/main/java/io/onedev/server/model/support/workspace/spec/template/CursorInShell.java
@@ -0,0 +1,40 @@
+package io.onedev.server.model.support.workspace.spec.template;
+
+import java.util.List;
+
+import io.onedev.commons.codeassist.InputSuggestion;
+import io.onedev.server.annotation.Editable;
+import io.onedev.server.model.support.workspace.spec.ShortcutConfig;
+import io.onedev.server.model.support.workspace.spec.WorkspaceSpec;
+import io.onedev.server.web.util.SuggestionUtils;
+
+@Editable(order=1400, name="nt:Cursor CLI In Shell", description="""
+        Create a workspace spec running Cursor CLI directly with server shell environment. 
+        Workspaces using created spec will share same environments and settings. 
+        To use this template, please ensure that: 
+        <ul>
+            <li><a href='https://cursor.com/docs/cli/overview' target='_blank'>Cursor CLI</a> is installed on OneDev server</li>
+            <li>A shell provisioner is added in 'Administration / Workspace Provisioners' with 'applicable projects' configured properly</li>
+        </ul>""")
+public class CursorInShell extends WorkspaceSpecTemplate {
+
+    @Override
+    public WorkspaceSpec createWorkspaceSpec() {
+        var workspaceSpec = new WorkspaceSpec();
+
+        workspaceSpec.setName(getName());
+        
+        var shortcutConfig = new ShortcutConfig();
+        shortcutConfig.setName("Cursor CLI");
+        shortcutConfig.setCommand("agent");
+        workspaceSpec.getShortcutConfigs().add(shortcutConfig);
+
+        return workspaceSpec;
+    }
+
+	@SuppressWarnings("unused")
+	private static List<InputSuggestion> suggestVariables(String matchWith) {
+		return SuggestionUtils.suggestWorkspaceVariables(matchWith);
+	}
+
+}
